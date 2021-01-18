576c61ffacebf254e865d5ee542847066af7d771
冯有俊 <feng.youjun@17cai.com>
2021-01-18 17:51:01 +0800

    init

---
+++ .history/src/views/assets/transLogs_20210118155859.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <public-header headerText="ENT记录"></public-header>
+    <div class="container white-bg">
+      <el-table :data="logsData" stripe class="mc-mtb-20">
+        <el-table-column label="币种" prop="from_coin"></el-table-column>
+        <el-table-column label="买入币种" prop="to_coin"></el-table-column>
+        <el-table-column label="买入量" prop="number"></el-table-column>
+        <!-- <el-table-column label="交易额" prop="address"></el-table-column> -->
+        <!-- <el-table-column label="状态" prop="status"></el-table-column> -->
+        <el-table-column
+          label="时间"
+          prop="created_at"
+          align="right"
+        ></el-table-column>
+      </el-table>
+      <div class="mc-flex mc-flex-justify-end mc-pb-20">
+        <el-pagination
+          :pager-count="5"
+          background
+          layout="prev, pager, next"
+          :total="1000"
+          @current-change="pageChange"
+        >
+        </el-pagination>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      logsData: [],
+      page: {
+        page: 1,
+        page_size: 20
+      }
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {
+    this.transLogs();
+  },
+
+  methods: {
+    async transLogs() {
+      await this.$axios
+        .get(`${this.$https.api}/user/exchangeList?page=${this.page.page}&page_size=${this.page.page_size}`)
+        .then((res) => {
+          if (res.status === 1) {
+            this.logsData = res.data;
+          } else {
+            this.logsData = [];
+          }
+        });
+    },
+    pageChange() {}
+  },
+};
+</script>
+<style lang='less' scoped>
+@import "~@/assets/css/public.less";
+</style>
---
+++ .history/src/views/team/team_20210118165750.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <public-header headerText="我的团队"></public-header>
+    <div class="container mc-mt-20 white-bg">
+      <div class="title">我的团队</div>
+      <div class="mc-p-20">
+        <el-tree :data="data" :props="defaultProps" accordion lazy 
+  :load="loadNode">
+          <!-- <div class="test">测hi是</div> -->
+        </el-tree>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {
+      data: [
+        {
+          name: "",
+        },
+      ],
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
+    };
+  },
+
+  components: {},
+
+  computed: {
+    ...mapState({
+      userInfo: "userInfo",
+    }),
+  },
+
+  mounted() {
+    this.data[0].name = this.userInfo.name;
+    this.getTeam();
+  },
+
+  methods: {
+    async getTeam() {
+      await this.$axios
+        .get(
+          `${this.$https.api}/user/myRecommend?page=1&page_size=10&user_id=${this.userInfo.id}`
+        )
+        .then((res) => {
+          console.log(res);
+        });
+    },
+    loadNode(){
+      
+    }
+  },
+};
+</script>
+<style lang='less' scoped>
+@import "~@/assets/css/public.less";
+.container {
+  min-height: 70vh;
+}
+.title {
+  height: 44px;
+  line-height: 44px;
+  padding: 0 20px;
+  background: @eee;
+}
+/deep/.el-tree {
+  .el-tree-node__content {
+    height: 44px;
+  }
+  .test {
+    line-height: 44px;
+  }
+}
+</style>
