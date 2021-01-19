45c9842349ae1662a32af48cf46fc6db1b97235e
冯有俊 <feng.youjun@17cai.com>
2021-01-19 14:06:28 +0800

    init

--- .history/src/views/login/login_20210119140547.vue
+++ .history/src/views/login/login_20210119140547.vue
@@ -64,7 +64,7 @@ export default {
         if (valid) {
           if (this.loginSwitch) {
             this.loginSwitch = false;
-            await this.$axios.post(this.$https.api + '/signIn', this.loginForm).then(res => {
+            await this.$axios.post(signIn', this.loginForm).then(res => {
               this.loginSwitch = true;
               if (res.status === 1) {
                 this.SET_USERINFO(res.data);
